87c1a0fdcf0f34ba0cab5e0cd974ac9bd551c120
Nicolas Schamne <schamne65@gmail.com>
2025-01-17 16:36:47 -0300

    segundo

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,4 +1,4 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
+/*import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
@@ -14,7 +14,7 @@ export default async function LatestInvoices({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: Uncomment this code in Chapter 7 */}
+        {/* NOTE: Uncomment this code in Chapter 7 }
 
         {/* <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
@@ -53,8 +53,8 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
-        <div className="flex items-center pb-2 pt-6">
+        </div> }
+     /*   <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
@@ -62,3 +62,4 @@ export default async function LatestInvoices({
     </div>
   );
 }
+*/
